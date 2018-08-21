5743f5da5e26d5f125da05f0304275fa09fa1140
Ricky Aguayo <raguayo911@gmail.com>
2018-08-20 22:30:58 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// Create a new component. This component should produce HTML
+
+const App = function() {
+    return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML and put it in the DOM
+React.render(App);
