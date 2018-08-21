a9c75739c5dac13c51d5846a3c7d5ea0c5c9126f
Ricky Aguayo <raguayo911@gmail.com>
2018-08-20 23:35:31 -0500

    added feature files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a new component. This component should produce HTML
 
-const App = function() {
+const App = () => {
     return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it in the DOM
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
